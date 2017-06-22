962e57d48216edd4a46094af1f32f0bf86cf3b52
ScStew <scottmstewart2@gmail.com>
2017-06-22 17:48:01 -0400

    fifth commit

--- Homework.rb
+++ Homework.rb
@@ -1,6 +1,9 @@
 100.times do |i|
 	i += 1
-	if (i % 3 == 0)
+	if (i % 5 == 0 && i % 3 == 0)
+		puts "Mined_Mines"
+	elsif 
+	 	 (i % 3 == 0)
     	puts "Mined"
     elsif 
     	(i % 5 == 0)
@@ -10,3 +13,4 @@
     end
   
 end
+ 
