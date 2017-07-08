88f4807a033f89869c06c7c644e6fffb34ea7683
ScStew <scottmstewart2@gmail.com>
2017-07-08 17:23:39 -0400

    function array setup

---
+++ mined_minds_kata_function_array.rb
@@ -0,0 +1,4 @@
+def kata_array(y)
+	arr = (0..100).to_a
+	arr[y]
+end
--- test_mined_minds_kata_function_array.rb
+++ test_mined_minds_kata_function_array.rb
@@ -1,7 +1,13 @@
 require "minitest/autorun"
 require_relative "mined_minds_kata_function_array"
 class Kata_array_test < Minitest::Test
+	
 	def test_1_equals_1
 		assert_equal(1,1)
 	end
+
+	def test_returns_1
+		assert_equal(1,kata_array(1))
+	end
+
 end
